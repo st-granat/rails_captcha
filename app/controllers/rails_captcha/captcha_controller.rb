d7006c933328d7761aa82e3a88eed65528f3e198
Andrey <railscode@gmail.com>
2012-06-04 00:38:09 +0700

    Выравнивание кода

--- app/controllers/rails_captcha/captcha_controller.rb
+++ app/controllers/rails_captcha/captcha_controller.rb
@@ -1,10 +1,10 @@
 # coding: utf-8
 class RailsCaptcha::CaptchaController < RailsCaptcha::ApplicationController
-	acts_as_captcha
+  acts_as_captcha
 
   def captcha
     reset_captcha
-		text = session[:captcha].blank? ? "" : session[:captcha]
+    text = session[:captcha].blank? ? "" : session[:captcha]
     render :text => text
   end
 
